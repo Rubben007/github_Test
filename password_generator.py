73fa60aaac8981b0a2cedf45caff5f3561786c6e
Rubben007 <139694884+Rubben007@users.noreply.github.com>
2023-11-08 06:38:00 +0100

    quick modification
    in this modification  the option to use uppercase, lowercase, digit and special characters were added

--- password_generator.py
+++ password_generator.py
@@ -3,12 +3,39 @@ import secrets
 import string
 
 
-def generat_password(length=12, use_uppercase=True, use_lowercase=True, use_specials=True, use_digit=True):
-    character = string.ascii_letters if use_uppercase else string.ascii_lowercase
+
+def generate_password(length=8, use_uppercase=True, use_lowercase=True, use_specials=True, use_digit=True):
+    combination = string.ascii_letters if use_uppercase else string.ascii_lowercase
     if use_digit:
-        character += string.digits
+        combination += string.digits
     if use_specials:
-        character += string.punctuation
+        combination += string.punctuation
     
-    password = "".join(secrets.choice(character) for _ in range(length))
+    password = "".join(secrets.choice(combination) for _ in range(length))
     return password
+
+if __name__ == "__main__":
+    try:
+        password_length = int(input("enter password_length: "))
+        use_uppercase = input("use_uppercase? (y/n): ").lower() == "y"
+        use_lowercase = input("use_lowercase? (y/n): ").lower() == "y"
+        use_digit = input("use_digit? (y/n): ").lower() == "y"
+        use_specials = input("use_specials? (y/n): ").lower() == "y"
+        
+        generated_password = generate_password(password_length, use_uppercase, use_lowercase, use_digit, use_specials)
+        print(f"Generated_password", {generated_password})
+    except ValueError:
+        print("enter a valid password length")
+
+if __name__ == "__main__":
+    try:
+        password_length = int(input("enter password_length: "))
+        use_uppercase = input("use_uppercase? (y/n): ").lower() == "y"
+        use_lowercase = input("use_lowercase? (y/n): ").lower() == "y"
+        use_digit = input("use_digit? (y/n): ").lower() == "y"
+        use_specials = input("use_specials? (y/n): ").lower() == "y"
+        
+        generated_password = generat_password(password_length, use_uppercase, use_lowercase, use_digit, use_specials)
+        print(f"Generated_password", generated_password)
+    except ValueError:
+        print("enter a valid password length")
